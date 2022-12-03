ade927faa8ac2de776b1e8f89d6445d89d82591a
Cichon Kendall <53377923+ckendall68@users.noreply.github.com>
2022-12-03 18:07:05 -0500

    Added listener for nav links clicked.

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,11 +8,18 @@ window.open("https://ckendall68.github.io/Portfolio/about.html");
 //window.open("https://www.google.com","_self")
 //}
 
-function showPage(page) {
-  var link = page;
- // var link = document.createElement("a")
-//  link.href = page; //"https://ckendall68.github.io/Portfolio/about.html"
- // link.target = "_self"
-  //link.click()
-window.location.assign(link);
+// function showPage(page) {
+//   var link = page;
+//  // var link = document.createElement("a")
+// //  link.href = page; //"https://ckendall68.github.io/Portfolio/about.html"
+//  // link.target = "_self"
+//   //link.click()
+// window.location.assign(link);
+
+var navItems = document.querySelectorAll(".nav-item");
+for (var i = 0; i < navItems.length; i++) {
+navItems[i].addEventListener("click", function() {
+   this.classList.add("active");
+});
 }
+console.log(navItems);
