0390f531db6020cc72d33c8bc6cfe0bcd244c3aa
Cichon Kendall <53377923+ckendall68@users.noreply.github.com>
2022-12-10 23:20:57 -0500

    Added code to display modals.

--- js/portfolio.js
+++ js/portfolio.js
@@ -26,12 +26,45 @@ window.open("https://ckendall68.github.io/Portfolio/about.html");
 // console.log(navItems);
 
 // Add active class to the current button (highlight it)
-var navbar = document.getElementById("collapsibleNavbar");
-var navLink = navbar.getElementsByClassName("nav-link");
-for (var i = 0; i < navLink.length; i++) {
-  navLink[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
+// var navbar = document.getElementById("collapsibleNavbar");
+// var navLink = navbar.getElementsByClassName("nav-link");
+// for (var i = 0; i < navLink.length; i++) {
+//   navLink[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace("active", "");
+//   this.className += " active";
+//   });
+// }
+function displayModal(modalName, btnName){
+  var mdl=modalName;
+  var mdlBtn=btnName;
+  console.log("shrug")
+  // Get the modal
+  var modal = document.getElementById(mdl);
+
+// Get the button that opens the modal
+var btn = document.getElementById(mdlBtn);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+}
+
+// displayModal(modalName,btnName);
